b309cc4b94d9b8557e59bd025c98acd6ebdde2f2
Max Persson <admin@cloudmodding.com>
2015-04-04 17:15:13 +0200

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,39 @@ var gulp        = require('gulp'),
     concat      = require('gulp-concat'),
     watch       = require('gulp-watch'),
     jade        = require('jade'),
-    gulpJade    = require('gulp-jade');
+    gulpJade    = require('gulp-jade'),
+    gutil       = require('gulp-util'),
+    source      = require('vinyl-source-stream'),
+    buffer      = require('vinyl-buffer'),
+    watchify    = require('watchify'),
+    browserify  = require('browserify'),
+    babelify    = require("babelify"),
+    aliasify    = require('aliasify');
+
+var mainBundler = watchify(browserify('./boot.js', watchify.args));
+// add any other browserify options or transforms here
+mainBundler.transform(babelify);
+mainBundler.transform(aliasify, {
+    aliases: {
+        "remote": "./shims/remote",
+        "fs": "./shims/fs",
+        "./lib/contextmenu": "./shims/contextmenu",
+    },
+});
+mainBundler.exclude('./lib/contextmenu.js');
+mainBundler.on('update', mainBundle); // on any dep update, runs the mainBundler
+mainBundler.on('log', gutil.log); // output build logs to terminal
+
+function mainBundle() {
+    return mainBundler.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('boot.js'))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+            .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('./browser'));
+}
 
 gulp.task('sass', function() {
     return sass('sass/style.scss', {sourcemap: true, style: 'compact'})
@@ -28,67 +60,29 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('browser', function() {
+gulp.task('watch-browser', function() {
+    mainBundle();
     var pattern;
     pattern = ['./main.jade'];
     gulp.src(pattern)
         .pipe(watch(pattern))
         .pipe(rename("index.html"))
         .pipe(gulpJade({
-          jade: jade,
-          pretty: true,
-          locals: {
-            'browser': true
-          }
+            jade: jade,
+            pretty: true,
+            locals: {
+                'browser': true
+            }
         }))
         .pipe(gulp.dest('browser'));
-
     pattern = ['./favicon.ico', 'FileSaver.min.js'];
     gulp.src(pattern).pipe(watch(pattern)).pipe(gulp.dest('browser'));
     pattern = ['./assets/**/*.*'];
     gulp.src(pattern).pipe(watch(pattern)).pipe(gulp.dest('browser/assets'));
 });
 
-gulp.task('watch', ['js', 'browser'], function () {
+gulp.task('watch-sass', function() {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-//Browserify test
-var gutil       = require('gulp-util');
-var source      = require('vinyl-source-stream');
-var buffer      = require('vinyl-buffer');
-var watchify    = require('watchify');
-var browserify  = require('browserify');
-var babelify    = require("babelify");
-var aliasify    = require('aliasify');
-
-var mainBundler = watchify(browserify('./boot.js', watchify.args));
-// add any other browserify options or transforms here
-mainBundler.transform(babelify);
-mainBundler.transform(aliasify, {
-    aliases: {
-        "remote": "./shims/remote",
-        "fs": "./shims/fs",
-        "./lib/contextmenu": "./shims/contextmenu",
-    },
-});
-mainBundler.exclude('./lib/contextmenu.js');
-mainBundler.on('update', mainBundle); // on any dep update, runs the mainBundler
-mainBundler.on('log', gutil.log); // output build logs to terminal
-
-function mainBundle() {
-  return mainBundler.bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('boot.js'))
-      .pipe(buffer())
-      .pipe(uglify())
-      .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-      .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./browser'));
-}
-
-gulp.task('js', function(){
-    mainBundle();
-});
-
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch-sass', 'watch-browser']);
