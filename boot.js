59990635c39a449deea2153a88b316ca66185da6
Max Persson <admin@cloudmodding.com>
2015-05-23 14:55:52 +0200

    Added support for multiple workspaces. Added popups. A whole bunch of other things.

--- boot.js
+++ boot.js
@@ -1,7 +1,7 @@
 var jade = require('jade');
 if (jade.renderFile) { // Browserify will not load jade and renderFile won't be present, skipping the code below.
     var html = jade.renderFile(__dirname+'/templates/interface.jade');
-    document.getElementById('app').innerHTML = html;
+    document.getElementById('app').innerHTML += html;
 }
 require("babel/register");
 require('cm-tree-view');
